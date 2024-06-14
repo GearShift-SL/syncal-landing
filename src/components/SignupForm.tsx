d3fe8d3ca999a3a9428dd6942e8a130f1c1fd507
Daniel Garcia Sanchez <dontic@zoho.com>
2024-06-14 11:09:33 +0000

    Update sign up form button

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -222,7 +222,7 @@ const SignupForm = () => {
               </div>
               <button
                 type="submit"
-                className="w-full bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full bg-[var(--aw-color-primary)] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
                 Create an account
               </button>
