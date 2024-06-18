395f9e5ae1e2c7d87e61b744a4fdc49ac5f1cc90
Daniel Garcia Sanchez <dontic@zoho.com>
2024-06-18 15:24:36 +0000

    Create sign up process

---
+++ src/pages/api/register.ts
@@ -0,0 +1,87 @@
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  if (request.headers.get("content-type") === "application/json") {
+    const body = await request.json();
+    const email = body.email;
+    const first_name = body.first_name;
+    const last_name = body.last_name;
+    const password = body.password;
+    const confirm_password = body.confirm_password;
+
+    console.log("Email:", email);
+
+    // Declares the Brevo API URL
+    const BREVO_API_URL = "https://api.brevo.com/v3/contacts";
+
+    // Gets the Brevo API Key from an environment variable
+    // Check the note on environment variables in the SSR section of this article to understand what is going on here
+    const BREVO_API_KEY =
+      import.meta.env.BREVO_API_KEY ?? process.env.BREVO_API_KEY;
+
+    // Just a simple check to make sure the API key is defined in an environment variable
+    if (!BREVO_API_KEY) {
+      console.error("No BREVO_API_KEY defined");
+      return new Response(null, { status: 400 });
+    }
+
+    // The payload that will be sent to Brevo
+    // This payload will create or update the contact and add it to the list with ID 3
+    const payload = {
+      updateEnabled: true,
+      email: email,
+      listIds: [3]
+    };
+
+    // Whatever process you want to do in your API endpoint should be inside a try/catch block
+    // In this case we're sending a POST request to Brevo
+    try {
+      // Make a POST request to Brevo
+      const response = await fetch(BREVO_API_URL, {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "api-key": BREVO_API_KEY,
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(payload)
+      });
+
+      // Check if the request was successful
+      if (response.ok) {
+        // Request succeeded
+        console.log("Contact added successfully");
+
+        // Return a 200 status and the response to our frontend
+        return new Response(
+          JSON.stringify({
+            message: "Contact added successfully"
+          }),
+          {
+            status: 200
+          }
+        );
+      } else {
+        // Request failed
+        console.error("Failed to add contact to Brevo");
+
+        // Return a 400 status to our frontend
+        return new Response(null, { status: 400 });
+      }
+    } catch (error) {
+      // An error occurred while doing our API operation
+      console.error(
+        "An unexpected error occurred while adding contact:",
+        error
+      );
+
+      // Return a 400 status to our frontend
+      return new Response(null, { status: 400 });
+    }
+  }
+
+  // If the POST request is not a JSON request, return a 400 status to our frontend
+  return new Response(null, { status: 400 });
+};
